1f004ff77ee5653fee69fb294fbe6708d6625a66
athapat-ct19038 <147899798+athapat-ct19038@users.noreply.github.com>
2024-02-26 12:07:04 +0530

    Update models.py

--- finder/models.py
+++ finder/models.py
@@ -2,18 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Company(models.Model):
+class Job(models.Model):
     id = models.AutoField(primary_key=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    company_name = models.CharField(max_length=30)
-    company_address = models.TextField()
-    company_bio = models.TextField(blank=True, null=True)
-    website = models.URLField()
-    contact_number = models.CharField(max_length=10)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
     email = models.EmailField()
-    type = models.CharField(max_length=15, default='company')
-
+    time_date = models.DateTimeField()
+    skill = models.CharField(max_length=255)
+    job_type = models.CharField(max_length=30)
+    location = models.CharField(max_length=100)
+    category = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.company_name
+        return self.title
 
