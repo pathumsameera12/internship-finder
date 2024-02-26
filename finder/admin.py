cb4cf0b1cd758c958b1c913b8de4477464c7948a
athapat-ct19038 <147899798+athapat-ct19038@users.noreply.github.com>
2024-02-26 12:33:26 +0530

    Update admin.py

--- finder/admin.py
+++ finder/admin.py
@@ -2,4 +2,19 @@ from django.contrib import admin
 from . models import *
 
 # Register your models here.
-admin.site.register(Company)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('title', 'job_id', 'time_date', 'job_type', 'location')  # Customize the fields displayed in the list view
+    list_filter = ('job_type', 'location')  # Add filters to the right sidebar
+    search_fields = ('title', 'description', 'location')  # Add a search bar
+    date_hierarchy = 'time_date'  # Add a date-based drilldown navigation by time_date
+
+    fieldsets = (
+        ('General Information', {
+            'fields': ('title', 'description', 'job_id')
+        }),
+        ('Job Details', {
+            'fields': ('email', 'time_date', 'skill', 'job_type', 'location')
+        }),
+    )
+
+admin.site.register(Job, JobAdmin)
